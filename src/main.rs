790894b949a331b61332e0ca9b18d661f074f7a1
Ryusei-0407 <ryu000407@gmail.com>
2022-01-17 00:53:37 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -4,53 +4,22 @@
 
 use core::intrinsics;
 use core::panic::PanicInfo;
-use x86_64::instructions::hlt;
-
-struct VGACell {
-    is_blinking: u1,
-    background_color: u3,
-    is_bright: u1,
-    character_color: u3,
-    character: u8,
-}
-
-#[repr(u8)]
-enum Color {
-    Black = 0,
-    Blue = 1,
-    Green = 2,
-    Cyan = 3,
-    Red = 4,
-    Magenta = 5,
-    Brown = 6,
-    Gray = 7,
-    White = 8,
-    BrightBlue = 9,
-    BrightGreen = 10,
-    BrightCyan = 11,
-    BrightRed = 12,
-    BrightMagenta = 13,
-    Yellow = 14,
-    DarkGray = 15,
-}
 
 #[panic_handler]
 #[no_mangle]
 pub fn panic(_info: &PanicInfo) -> ! {
-    unsafe {
-        intrinsics::abort();
-    }
+  intrinsics::abort();
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let mut framebuffer = 0xb8000 as *mut u8;
+  let framebuffer = 0xb8000 as *mut u8;
 
-    unsafe {
-        framebuffer.offset(1).write_volatile(0x30); // *(framebuffer + 1) = 0x30;
-    }
+  unsafe {
+    framebuffer
+      .offset(1)
+      .write_volatile(0x30);
+  }
 
-    loop {
-        hlt();
-    }
+  loop {}
 }

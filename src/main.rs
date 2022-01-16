1a5fb05e5ba47f0448e0afab9c9c000f7073a023
Ryusei-0407 <ryu000407@gmail.com>
2022-01-16 23:42:11 +0900

    VGA compatible and color

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,48 @@
 use core::intrinsics;
 use core::panic::PanicInfo;
 
+struct VGACell {
+    is_blinking: u1,
+    background_color: u3,
+    is_bright: u1,
+    character_color: u3,
+    character: u8,
+}
+
+#[repr(u8)]
+enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    Gray = 7,
+    White = 8,
+    BrightBlue = 9,
+    BrightGreen = 10,
+    BrightCyan = 11,
+    BrightRed = 12,
+    BrightMagenta = 13,
+    Yellow = 14,
+    DarkGray = 15,
+}
+
 #[panic_handler]
 #[no_mangle]
 pub fn panic(_info: &PanicInfo) -> ! {
-    intrinsics::abort();
+    unsafe {
+        intrinsics::abort();
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let framebuffer = 0xb8000 as *mut u8;
+    let mut framebuffer = 0xb8000 as *mut u8;
 
     unsafe {
-        framebuffer.offset(1).write_volatile(0x30);
+        framebuffer.offset(1).write_volatile(0x30); // *(framebuffer + 1) = 0x30;
     }
 
     loop {}
